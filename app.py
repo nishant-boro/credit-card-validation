44a7c0c56ba4ad95f2dbcc224fbe160dd0b5d1df
Nishant Boro <nishant4317@gmail.com>
2021-03-05 03:00:45 +0530

    added travis and coverall

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import validator
 
 app = Flask(__name__)
@@ -7,8 +7,30 @@ app.secret_key = "abcasdasdasdas"
 
 @app.route('/validate', methods=['GET'])
 def validate():
+    no = request.headers.get('no')
+    month = request.headers.get('month')
+    year = request.headers.get('year')
+    cvc = request.headers.get('cvc')
+
+    if no is None:
+        return jsonify({"error": "no not provided"}), 400
     
-    return response()
+    if month is None:
+        return jsonify({"error": "month not provided"}), 400
+
+    if year is None:
+        return jsonify({"error": "year is not provided"}), 400
+
+    if cvc is None:
+        return jsonify({"error": "cvc is not provided"}), 400
+
+    card = validator.Card(no, int(month), int(year), int(cvc))
+
+    if card.is_valid:
+        return jsonify({"valid": "true", "brand": card.brand})
+    else:
+        return jsonify({"valid": "false"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tests.py
@@ -0,0 +1,17 @@
+import unittest
+import app
+import json
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+        self.headers = {'Content-Type': 'application/json'}
+
+    def test_validation(self):
+        self.headers['no'] = '370000000000002'
+        self.headers['month'] = '02'
+        self.headers['year'] = '2030'
+        self.headers['cvc'] = '7373'
+        response = self.app.get("/validate", headers=self.headers)
+        json_response = json.loads(response.get_data(as_text=True))
+        self.assertEqual(json_response['valid'], 'true')
